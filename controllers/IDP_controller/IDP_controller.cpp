4a1a9f116d2ea511423823f7c13fc7e70976af5c
rageoveralostpenny <wu.cindyx@gmail.com>
2021-03-09 23:40:25 +0000

    GPS troubleshooted

--- controllers/IDP_controller/IDP_controller.cpp
+++ controllers/IDP_controller/IDP_controller.cpp
@@ -22,27 +22,17 @@ Motor *leftArmMotor = robot->getMotor("Arm_L");
 Motor *rightArmMotor = robot->getMotor("Arm_R");
 GPS *gps = robot->getGPS("gps");
 Compass *compass = robot->getCompass("compass");
-/*LightSensor *lightSensor = robot->getLightSensor("lightsensor");
-lightSensor->enable(TIME_STEP);
-*/
-
-//Initialsing US and IR sensors
+LightSensor *lightSensor = robot->getLightSensor("TEPT4400");
 DistanceSensor *ds[3];
 char dsNames[3][20] = {"us_right","us_left","ir"};
 
 //Initiate vector to store sensor readings
 std::vector<std::vector<double>> sensorValueScan;
 
-void rotate(double theta){
 
-// set the target position of the motors
-  leftMotor->setPosition(theta);
-  rightMotor->setPosition(-theta);
-  
-  // set up the motor speeds at 50% of the MAX_SPEED.
-   leftMotor->setVelocity(0.5 * MAX_SPEED);
-   rightMotor->setVelocity(0.5 * MAX_SPEED);
-}
+/*==================================================
+MOTION FUNCTIONS
+===================================================*/
 
 void move_forwards() {
 
@@ -135,46 +125,152 @@ std::vector<double> getSensorValues(double (*get_bearing_in_degrees)()){
     sensorValues[3] = bearing;
     return sensorValues;
 }
-std::vector<double> getBlockBearings(){
+<<<<<<< Updated upstream
+=======
 
-      std::vector<double> blockBearings;
-      
-      //Calculating average returned IR sensor value
-      int lookUpTableSize = ds[2]->getLookupTableSize();
-      int numCounter = 0;
-      double sumDistance = 0;
-      for(int j = 0; j<lookUpTableSize;j++){
-        sumDistance += sensorValueScan[j][2];
-        if(sensorValueScan[j][2]!=0){
-          numCounter++;
-        }
-      }
-      double avgDistance = sumDistance/numCounter;
-      
-      
-      //Alpha is a shortcode for the distance value on the jth row, 2nd column of sensorValueScam
-      double alpha;
-      //Conditional logic to extract bearings and then convert to GPS location
-      for(int j = 2; j<lookUpTableSize;j++){
-        alpha = sensorValueScan[j][2];
-        /*Conditions for blocks to be picked out:
-        1. Large jump between previous value
-        2. Large jump between value two time steps previously
-        3. Large difference between distance value recorded and average distance value
-        calculated above
-        */
-        if( (alpha - sensorValueScan[j-1][2]) > 0.15 || 
-        (alpha - sensorValueScan[j-2][2]) > 0.15 || 
-        (avgDistance - alpha) > 0.3){
-          blockBearings.push_back(sensorValueScan[j][3]);
-        }
-      }
-      
-      return blockBearings;
+std::vector<double> getRawSensorValues(double (*get_bearing_in_degrees)()){
+
+    //Creating vector to store one row of sensor values for one time step
+    std::vector<double> sensorValues(4);
+    
+    //Input distance sensor values as distance (involves dividing through by 57)
+    for (int i = 0; i < 3; i++){
+      sensorValues[i]=ds[i]->getValue();
+    }
+    return sensorValues;
 }
-//Clean sensorValueScan to get block bearings
-std::vector<std::vector<double>> getBlockGPS(){
+      
+double get_bearing_in_degrees() {
+    const double *north = compass->getValues();
+    double rad = atan2(north[0], north[2]);
+    double bearing = rad / M_PI * 180.0;
+    if (bearing < 0.0){
+      bearing = bearing + 360.0;
+    }
+    return bearing;
+}
 
+/*======================================================================
+SCANNING FUNCTIONS
+========================================================================*/
+
+//Function to rotate by a fixed angle
+void rotate_theta(double theta, double initial_bearing, bool &fin) { 
+
+    double angle_rotated = 0.0;
+    // set the target position, velocity of the motors
+    leftMotor->setPosition(INFINITY);
+    rightMotor->setPosition(INFINITY);
+    leftMotor->setVelocity(0.5 * MAX_SPEED);
+    rightMotor->setVelocity(-0.5 * MAX_SPEED);
+    
+    while (robot->step(TIME_STEP) != -1){
+        double bearing = get_bearing_in_degrees();
+        if ((bearing - initial_bearing) >= 0.0) {
+        angle_rotated = bearing - initial_bearing;
+    }
+    
+    if ((bearing - initial_bearing) < 0.0) {
+      angle_rotated = bearing + (360.0 - initial_bearing);
+    } 
+    //std::cout << angle_rotated << std::endl;
+    //std::cout << theta << std::endl;
+    
+    if (angle_rotated > theta) {
+       leftMotor->setVelocity(0.0 * MAX_SPEED);
+       rightMotor->setVelocity(0.0 * MAX_SPEED);
+       fin = true;
+       break;
+    }
+  } 
+}
+
+//Version of rotate_theta that also scans while doing so
+void doScan(double theta, double initial_bearing, bool *fin) { 
+
+    double angle_rotated = 0.0;
+    // set the target position, velocity of the motors
+    leftMotor->setPosition(INFINITY);
+    rightMotor->setPosition(INFINITY);
+    leftMotor->setVelocity(0.5 * MAX_SPEED);
+    rightMotor->setVelocity(-0.5 * MAX_SPEED);
+    
+    while (robot->step(TIME_STEP) != -1){
+        double bearing = get_bearing_in_degrees();
+        std::vector<double> Values = getSensorValues(get_bearing_in_degrees);
+        sensorValueScan.push_back(Values);
+        if ((bearing - initial_bearing) >= 0) {
+        angle_rotated = bearing - initial_bearing;
+        }
+    
+        if ((bearing - initial_bearing) < 0) {
+          angle_rotated = bearing + (360 - initial_bearing);
+        } 
+        //std::cout << angle_rotated << std::endl;
+        //std::cout << theta << std::endl;
+    
+        if (angle_rotated > theta) {
+           leftMotor->setVelocity(0);
+           rightMotor->setVelocity(0);
+           *fin = true;
+           break;
+        }
+   } 
+}
+
+void rotate_until_bearing(double target_bearing, double initial_bearing) {
+
+    double angle_rotated = 0.0;
+    // set the target position, velocity of the motors
+    leftMotor->setPosition(INFINITY);
+    rightMotor->setPosition(INFINITY);
+    
+    if (target_bearing > initial_bearing) {
+      leftMotor->setVelocity(0.1 * MAX_SPEED);
+      rightMotor->setVelocity(-0.1 * MAX_SPEED);
+      
+      while (robot->step(TIME_STEP) != -1){
+        double bearing = get_bearing_in_degrees();
+        //std::cout << bearing << std::endl;
+        
+        if (bearing > target_bearing) {
+          leftMotor->setVelocity(0.0 * MAX_SPEED);
+          rightMotor->setVelocity(0.0 * MAX_SPEED);
+          break;
+          
+        }
+      }
+    }
+    
+    if (target_bearing < initial_bearing) {
+    
+      leftMotor->setVelocity(-0.1 * MAX_SPEED);
+      rightMotor->setVelocity(0.1 * MAX_SPEED);
+      
+      while (robot->step(TIME_STEP) != -1){
+        double bearing = get_bearing_in_degrees();
+        //std::cout << bearing << std::endl;
+        
+        if (bearing < target_bearing) {
+          leftMotor->setVelocity(0.0 * MAX_SPEED);
+          rightMotor->setVelocity(0.0 * MAX_SPEED);
+          break;
+          
+        }
+      }
+    }
+}
+
+/*================================================================
+BLOCK DETECTION 
+==================================================================*/
+
+//Takes uncleaned 2d vector and filters for blocks, returning array of their bearings
+>>>>>>> Stashed changes
+std::vector<double> getBlockBearings(){
+      
+      std::cout << "function call successful" << std::endl;
+      
       std::vector<double> blockBearings;
       std::vector<double> blockDistances;
       std::vector<std::vector<double>> blockGPS;
@@ -183,43 +279,119 @@ std::vector<std::vector<double>> getBlockGPS(){
       int lookUpTableSize = ds[2]->getLookupTableSize();
       int numCounter = 0;
       double sumDistance = 0;
-      for(int j = 0; j<lookUpTableSize;j++){
-        sumDistance += sensorValueScan[j][2];
-        if(sensorValueScan[j][2]!=0){
+      for (unsigned int i =0; i<sensorValueScan.size();i++){
+        sumDistance += sensorValueScan[i][2];
+        if(sensorValueScan[i][2]!=0){
           numCounter++;
         }
       }
+      
+      std::cout << numCounter << std::endl;
+      std::cout << "numCounter print you lil bitch" << std::endl;
+      
       double avgDistance = sumDistance/numCounter;
       
       //Alpha is a shortcode for the distance value on the jth row, 2nd column of sensorValueScam
       double alpha;
       //Conditional logic to extract bearings and then convert to GPS location
+      for(unsigned int i = 1; i<sensorValueScan.size();i++){
+        alpha = sensorValueScan[i][2];
+        /*Conditions for blocks to be picked out:
+        1. Large jump between previous value
+        2. Large difference between distance value recorded and average distance value
+        calculated above
+        */
+<<<<<<< Updated upstream
+        if( (alpha - sensorValueScan[j-1][2]) > 0.15 || 
+        (alpha - sensorValueScan[j-2][2]) > 0.15 || 
+        (avgDistance - alpha) > 0.3){
+          blockBearings.push_back(sensorValueScan[j][3]);
+=======
+        if( (alpha-sensorValueScan[i-1][2]) > 0.05 
+        //|| (avgDistance-alpha) > 0.3
+        ){
+          blockBearings.push_back(sensorValueScan[i][3]);
+>>>>>>> Stashed changes
+        }
+      }
+      
+      return blockBearings;
+}
+//Clean sensorValueScan to get block bearings
+std::vector<std::vector<double>> getBlockGPS(){
+      
+      std::cout << "function call successful" << std::endl;
+      
+      std::vector<double> blockBearings(8);
+      std::vector<double> blockDistances(8);
+      
+      //Calculating average returned IR sensor value
+      int lookUpTableSize = ds[2]->getLookupTableSize();
+      int numCounter = 0;
+      double sumDistance = 0;
+<<<<<<< Updated upstream
+      for(int j = 0; j<lookUpTableSize;j++){
+        sumDistance += sensorValueScan[j][2];
+        if(sensorValueScan[j][2]!=0){
+=======
+      for (unsigned int i =0; i<sensorValueScan.size();i++){
+        sumDistance += sensorValueScan[i][2];
+        if(sensorValueScan[i][2]!=0){
+>>>>>>> Stashed changes
+          numCounter++;
+        }
+      }
+      
+      std::cout << numCounter << std::endl;
+      std::cout << "numCounter print you lil bitch" << std::endl;
+      
+      double avgDistance = sumDistance/numCounter;
+      std::vector<std::vector<double>> blockGPS(8, std::vector<double>(3));
+      
+      //Alpha is a shortcode for the distance value on the jth row, 2nd column of sensorValueScam
+      double alpha;
+      int j=0;
+      //Conditional logic to extract bearings and then convert to GPS location
+<<<<<<< Updated upstream
       for(int j = 2; j<lookUpTableSize;j++){
         alpha = sensorValueScan[j][2];
+=======
+      for(unsigned int i = 1; i<sensorValueScan.size();i++){
+        alpha = sensorValueScan[i][2];
+>>>>>>> Stashed changes
         /*Conditions for blocks to be picked out:
         1. Large jump between previous value
         2. Large jump between value two time steps previously
         3. Large difference between distance value recorded and average distance value
         calculated above
         */
+<<<<<<< Updated upstream
         if( (alpha - sensorValueScan[j-1][2]) > 0.15 || 
         (alpha - sensorValueScan[j-2][2]) > 0.15 || 
         (avgDistance - alpha) > 0.3){
           blockBearings.push_back(sensorValueScan[j][3]);
           blockDistances.push_back(alpha);
+=======
+        if( (alpha-sensorValueScan[i-1][2]) > 0.05 || (avgDistance-alpha) > 0.3){
+          blockBearings[j]=sensorValueScan[i][3];
+          blockDistances[j]=alpha;
+          j++;
+          
+>>>>>>> Stashed changes
         }
+        
       }
       
       //Turning bearings and distances into new GPS readings
-      const double *position = gps->getValues();
-      for (auto i : blockBearings){
-        blockGPS[i][0] = position[0]+(blockDistances[i]+0.12)*cos(blockBearings[i]*M_PI/180);
-        blockGPS[i][1] = position[1];
-        blockGPS[i][2] = position[2]+(blockDistances[i]+0.12)*sin(blockBearings[i]*M_PI/180);
+      for (int i=0; i<8; i++){
+        blockGPS[i][0] = gps->getValues()[0]+(blockDistances[i]+0.12)*cos(blockBearings[i]*M_PI/180);
+        blockGPS[i][1] = gps->getValues()[1];
+        blockGPS[i][2] = gps->getValues()[2]+(blockDistances[i]+0.12)*sin(blockBearings[i]*M_PI/180);
       }
       return blockGPS;
 }
 
+<<<<<<< Updated upstream
       
 double get_bearing_in_degrees() {
     const double *north = compass->getValues();
@@ -299,13 +471,26 @@ void rotate_until_bearing(double target_bearing, double initial_bearing) {
 
 //std::vector<double> findBlocks(std::vector<double> &dsValueScan);
    
+=======
+/*=================================================================
+GET COLOUR
+==================================================================*/
+/*bool getColour(){
+
+      double irradiance = lightSensor->getValue();
+      
+}*/
+/*================================================================
+MAIN FUNCTION
+==================================================================*/
+ 
+>>>>>>> Stashed changes
 int main(int argc, char **argv) {
     
-    //Enabling navigation
+    //Enabling devices
     gps->enable(TIME_STEP);
     compass->enable(TIME_STEP);
-  
-    //Retrieving device tags and enabling with refresh time step
+    lightSensor->enable(TIME_STEP);
     for(int i = 0; i < 3; i++){
       ds[i] = robot->getDistanceSensor(dsNames[i]);
       ds[i]->enable(TIME_STEP); //TIME_STEP defines rate at which sensor is refreshed
@@ -322,6 +507,7 @@ int main(int argc, char **argv) {
    bool fin2 = false;
    
    while (robot->step(TIME_STEP) != -1){
+<<<<<<< Updated upstream
       
       //Initial scan
       if(fin1==false){
@@ -335,13 +521,51 @@ int main(int argc, char **argv) {
       if(fin1==true && fin2==false){
         std::vector<double> blockBearings = getBlockBearings();
         rotate_until_bearing(blockBearings[0],get_bearing_in_degrees());
+=======
+       
+       //Code snippet to get sensor values as bot rotates in circle
+       /*std::vector<double> Values = getRawSensorValues(get_bearing_in_degrees);
+       sensorValueScan.push_back(Values);
+       rotate_ACW();*/
+       
+       //Initial scan
+       if(fin1==false){
+          double currentBearing = get_bearing_in_degrees();
+          doScan(355, currentBearing, &fin1);
+          std::cout << "Crashing is not because of DoScan" << std::endl;
+       }
+      
+      
+      /*if(fin2==false){
+         std::vector<double> blockBearings = getBlockBearings();
+         for(auto& i:blockBearings){
+             std::cout << i << std::endl;
+            }
+         std::vector<std::vector<double>> GPSOfBlocks = getBlockGPS();
+         for(auto& row:GPSOfBlocks){
+           for(auto& i:row){
+              std::cout << i << std::endl;
+           }
+           std::cout << "NEXT" << std::endl;
+         }
+         fin2 = true;
+      }*/
+      
+      if(robot->step(TIME_STEP) != -1){
+        break;
+      }
+      
+      //Collecting one block
+      if(fin1==true && fin2==false){
+        std::vector<double> bearings = getBlockBearings();
+        rotate_until_bearing(bearings[0],get_bearing_in_degrees());
+>>>>>>> Stashed changes
         open_arms();
-        std::vector<std::vector<double>> blockGPS = getBlockGPS();
+        std::vector<std::vector<double>> GPSOfBlocks = getBlockGPS();
         
         do{
-          const double *currentLocation = gps->getValues();
-          double delta_x = fabs(blockGPS[0][0]-currentLocation[0]);
-          double delta_z = fabs(blockGPS[0][2]-currentLocation[2]);
+          double delta_x = fabs(GPSOfBlocks[0][0]-gps->getValues()[0]);
+          double delta_z = fabs(GPSOfBlocks[0][2]-gps->getValues()[2]);
           double distance = sqrt(delta_x*delta_x + delta_z*delta_z);
           
           leftMotor->setPosition(INFINITY);
@@ -360,6 +584,13 @@ int main(int argc, char **argv) {
         close_arms();
         fin2 = 1;
        }
+<<<<<<< Updated upstream
+=======
+
+/*=========================================
+CODE SNIPPET TO OUTPUT SENSOR VALUE 2D ARRAY
+===========================================*/
+>>>>>>> Stashed changes
       
       //Outputting sensorValueScan 
       for(int j = 0; j<4; j++){
@@ -385,7 +616,11 @@ int main(int argc, char **argv) {
       
       i++;
       
+<<<<<<< Updated upstream
       if(i==999){
+=======
+      if(i==2){
+>>>>>>> Stashed changes
         break;
        }
        
@@ -428,8 +663,9 @@ CODE SNIPPET FOR OUTPUTTING GPS AND COMPASS VALUES
       //std::cout << i << std::endl;
       //i++;
       */
-   }
       
+   } //bracket to end while loop
+   
    delete robot;
    return 0;
  }
